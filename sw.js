24dd7b5c8e97d1c9cf80eca716f4b0251355d96a
Sukhoronchak <152394451+Sukhoronchak@users.noreply.github.com>
2026-03-27 11:20:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,16 @@
-self.addEventListener('install', (e) => {
+// sw.js - Сервіс-воркер для активації PWA функцій
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Просто пропускаємо запити через мережу
+    event.respondWith(fetch(event.request));
+});self.addEventListener('install', (e) => {
   console.log('Service Worker installed');
 });
 
